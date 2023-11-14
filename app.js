5ac0c6e1085cdb5583f303fc3749a5b038bcfdce
EdMorales2986 <ed.morales6892@gmail.com>
2023-11-13 21:46:14 -0400

    TeamWork :+1:

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-const express = require ('express')
-const cors = require ('cors')
+const express = require("express");
+const cors = require("cors");
+const cluster = require("node:cluster");
+
 const port = 4000;
 
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use(require('./src/routes/routes'))
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
 
-app.listen(port, (req, res) => {
-    console.log('servidor en ',port)
-})
+  for (let i = 0; i < 3; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  app.use(require("./src/routes/routes"));
 
-
-
-
+  app.listen(port, (req, res) => {
+    console.log("worker" + `\u001b[1;31m ${process.pid}` + "\u001b[0m started");
+  });
+}
--- src/controllers/read.js
+++ src/controllers/read.js
@@ -1,12 +1,11 @@
 const client = require("../../gRPC_cli");
 
-function read (req, res) {
- client.read({}, function(err, response) {
-    console.log(response)
-    res.status(200).json(response)
- }
-    )
-
+function read(req, res) {
+  client.read({}, function (err, response) {
+    //  console.log(response)
+    console.log(process.pid);
+    res.status(200).json(response);
+  });
 }
 
-module.exports = read
+module.exports = read;
