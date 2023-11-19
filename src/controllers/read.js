e3e79fb4318b0690d97d7225bbd20ab6a36384fa
EdMorales2986 <ed.morales6892@gmail.com>
2023-11-18 20:14:18 -0400

    idk something

--- src/controllers/read.js
+++ src/controllers/read.js
@@ -5,8 +5,12 @@ function read(req, res) {
   const worker = new Worker("./src/controllers/workerRead.js");
 
   worker.on("message", (response) => {
-    // do something after completion of task
-    // console.log("\u001b[1;32mDone");
+    // do something after end of task
+    // const time = new Date();
+    // console.log(
+    //   `\u001b[1;32mDone \u001b[0m${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`
+    // );
+    // console.log(worker.threadId);
     res.status(200).json(response);
   });
 
